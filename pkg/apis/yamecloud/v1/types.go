3a9df80f1f0986a5d611311bc9448ca34ff46c78
laik <laik.lj@me.com>
2022-04-23 14:12:41 +0800

    remove etcd and add reuse ip

--- pkg/apis/yamecloud/v1/types.go
+++ pkg/apis/yamecloud/v1/types.go
@@ -1,7 +1,10 @@
 package v1
 
 import (
+	"net"
 	"sort"
+	"strconv"
+	"strings"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -20,6 +23,49 @@ type IPPoolSpec struct {
 	Ips map[string]IPList `json:"ips"`
 }
 
+func UInt32ToIP(intIP uint32) string {
+	var bytes [4]byte
+	bytes[0] = byte(intIP & 0xFF)
+	bytes[1] = byte((intIP >> 8) & 0xFF)
+	bytes[2] = byte((intIP >> 16) & 0xFF)
+	bytes[3] = byte((intIP >> 24) & 0xFF)
+	return net.IPv4(bytes[3], bytes[2], bytes[1], bytes[0]).String()
+}
+
+func IPStringToUInt32(ip string) uint32 {
+	bits := strings.Split(ip, ".")
+	b0, _ := strconv.Atoi(bits[0])
+	b1, _ := strconv.Atoi(bits[1])
+	b2, _ := strconv.Atoi(bits[2])
+	b3, _ := strconv.Atoi(bits[3])
+	var sum uint32
+	sum += uint32(b0) << 24
+	sum += uint32(b1) << 16
+	sum += uint32(b2) << 8
+	sum += uint32(b3)
+	return sum
+}
+
+func (spec IPPoolSpec) Reuse() string {
+	ips := make(IPList, 0)
+	for _, i := range spec.Ips {
+		ips = append(ips, i...)
+	}
+	sort.StringSlice(ips).Sort()
+	reuse := func(start, end string) string {
+		startIp := IPStringToUInt32(start)
+		endIp := IPStringToUInt32(end)
+		for startIp <= endIp {
+			if !spec.FindIp(UInt32ToIP(startIp)) {
+				return UInt32ToIP(startIp)
+			}
+			startIp++
+		}
+		return ""
+	}
+	return UInt32ToIP(IPStringToUInt32(reuse(ips[0], ips[len(ips)-1])) - 1)
+}
+
 func (spec IPPoolSpec) Last() string {
 	ips := make(IPList, 0)
 	for _, i := range spec.Ips {
@@ -31,6 +77,17 @@ func (spec IPPoolSpec) Last() string {
 
 func (spec IPPoolSpec) Release(id string) {
 	delete(spec.Ips, id)
+}
+
+func (spec IPPoolSpec) FindIp(ip string) bool {
+	for _, ipList := range spec.Ips {
+		for _, i := range ipList {
+			if i == ip {
+				return true
+			}
+		}
+	}
+	return false
 }
 
 func (spec IPPoolSpec) Find(id, ip string) bool {
